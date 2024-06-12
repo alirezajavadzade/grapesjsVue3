eb38804750289169063015b8d452590173442e19
Alireza Javadzade <alirezajavadzade76@gmail.com>
2024-06-12 17:36:09 +0330

    portrait device & api store

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,19 +12,19 @@ import grapesjsPluginForms from 'grapesjs-plugin-forms';
 import grapesjsPluginExport from 'grapesjs-plugin-export';
 import grapesjsPresetWebpage from 'grapesjs-preset-webpage';
 import grapesjsTemplates from 'grapesjs-templates';
-import grapesjsCustomCode from 'grapesjs-custom-code';
 import grapesjsIndexeddb from 'grapesjs-indexeddb';
 import fa from 'grapesjs/locale/fa';
 import grapesjsRulers from 'grapesjs-rulers'
 import 'grapesjs-rulers/dist/grapesjs-rulers.min.css'
+import axios from 'axios';
 
 const editor = ref(null);
 const gjs = ref();
+const projectEndpoint = "http://172.16.1.63:3500/template";
+const currentId = ref('0');
 
 
 onMounted(() => {
-
-  let x = document.querySelector('.gjs-pn-buttons')
 
 
   editor.value = grapesjs.init({
@@ -39,23 +39,39 @@ onMounted(() => {
       devices: [{
         name: 'A3',
         width: '1122px',
-        widthMedia: '793px',
         height: '793px',
       }, {
         name: 'A4',
         width: '793px',
-        widthMedia: '1122px',
         height: '1122px',
       },
       {
         name: 'A5',
         width: '561px',
-        widthMedia: '793px',
         height: '793px',
       },
       ]
     },
-
+    commands: {
+      defaults: [
+        {
+          id: 'portrait-visibility',
+          run() {
+            let x = document.querySelector('.gjs-frame-wrapper')
+            let w = x.style.width
+            let h = x.style.height
+            x.style.width = h
+            x.style.height = w
+          },
+        },
+        {
+          id: 'save-data',
+          run() {
+            console.log('x')
+          }
+        }
+      ],
+    },
     i18n: {
       locale: 'fa',
       localeFallback: 'fa',
@@ -64,41 +80,35 @@ onMounted(() => {
     selectorManager: {
       componentFirst: true,
     },
+    // storageManager: {
+    // type : "local",
+    // autosave: true,
+    // autoload: true,
+    // options: {
+    //   local: {
+    //     key: 'alireza',
+    //   },
+    // }
+    // },
+
+
     storageManager: {
-      type: "indexeddb",
+      type: 'remote',
       autosave: true,
-      autoload: true,
-
-
-      // type : "local",
-      // autosave: true,
-      // autoload: true,
-      // options: {
-      //   local: {
-      //     key: 'gjsProject',
-      //   },
-      // }
-
-
-      // type: 'rest-api', 
-      // options: {
-      //   remote: {
-      //     urlStore: 'https://endpoint/store/',// POST
-      //     urlLoad: 'https://endpoint/load/',// GET
-      //     urlDelete: 'https://endpoint/delete/',// DELETE
-      //     // ...
-      //   }
-      // }
+      contentTypeJson: true,
+      stepsBeforeSave: 3,
+      storeComponents: true,
+      storeStyles: true,
+      storeHtml: true,
+      storeCss: true,
     },
-    plugins: [gjsBlocksBasic, grapesjsPluginForms, grapesjsPluginExport, grapesjsPresetWebpage, grapesjsCustomCode, grapesjsIndexeddb, grapesjsTemplates, grapesjsRulers],
+
+
+    plugins: [gjsBlocksBasic, grapesjsPluginForms, grapesjsPluginExport, grapesjsPresetWebpage, grapesjsIndexeddb, grapesjsTemplates, grapesjsRulers],
     pluginsOpts: {
       gjsBlocksBasic: { flexGrid: true, blocks: ['text', 'link', 'image', 'video', 'map'] },
       grapesjsIndexeddb: {
-        options: {
-          key: 'user-project-id',
-          dbName: 'editorLocalData',
-          objectStoreName: 'projects',
-        }
+        options: { key: 'gjsProject', dbName: 'gjs', objectStoreName: 'projects' }
       },
       grapesjsPresetWebpage: {
         modalImportTitle: 'Import Template',
@@ -107,8 +117,9 @@ onMounted(() => {
           return editor.getHtml() + '<style>' + editor.getCss() + '</style>'
         },
         grapesjsTemplates: {
-          templates: 'http://localhost:5173/templates',
-          projects: 'http://localhost:5173/projects',
+          // templates: 'http://172.16.1.63:3500/template',
+          // projects: 'http://172.16.1.63:3500/template',
+          onStore: 'http://172.16.1.63:3500/template'
         },
       },
     },
@@ -134,7 +145,7 @@ onMounted(() => {
           activate: true,
         }
       ],
-    }
+    },
   });
 
 
@@ -145,6 +156,8 @@ onMounted(() => {
   const panelViews = pn.addPanel({
     id: 'options'
   });
+
+
   panelViews.get('buttons').add([{
     attributes: {
       title: 'Toggle Rulers'
@@ -155,6 +168,56 @@ onMounted(() => {
     id: 'ruler-visibility'
   }]);
 
+
+  panelViews.get('buttons').add([{
+    attributes: {
+      title: 'Toggle Portrait'
+    },
+    context: 'toggle-portrait',
+    label: `<svg fill="#000000" width="18" height="18" viewBox="0 0 36 36" version="1.1"  preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><title>landscape-line</title><path d="M32,6H4A2,2,0,0,0,2,8V28a2,2,0,0,0,2,2H32a2,2,0,0,0,2-2V8A2,2,0,0,0,32,6Zm0,22H4V8H32Z" class="clr-i-outline clr-i-outline-path-1"></path><path d="M9,22a.82.82,0,0,0,.55-.21.8.8,0,0,0,0-1.13L7.83,18.8H28.17l-1.72,1.86a.8.8,0,0,0,0,1.13A.82.82,0,0,0,27,22a.78.78,0,0,0,.58-.26L31.09,18l-3.47-3.74a.79.79,0,0,0-1.13,0,.8.8,0,0,0,0,1.13l1.72,1.86H7.83l1.72-1.86a.8.8,0,0,0,0-1.13.79.79,0,0,0-1.13,0L4.91,18l3.47,3.74A.78.78,0,0,0,9,22Z" class="clr-i-outline clr-i-outline-path-2"></path><rect x="0" y="0" width="36" height="36" fill-opacity="0"/></svg>`,
+    command: 'portrait-visibility',
+    id: 'portrait-visibility'
+  }]);
+
+
+  panelViews.get('buttons').add([{
+    attributes: {
+      title: 'save data'
+    },
+    context: 'save-data',
+    label: `<svg width="18" height="18" viewBox="0 -0.5 21 21" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    
+    <title>save_item [#1411]</title>
+    <desc>Created with Sketch.</desc>
+    <defs>
+
+</defs>
+    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <g id="Dribbble-Light-Preview" transform="translate(-419.000000, -640.000000)" fill="#000000">
+            <g id="icons" transform="translate(56.000000, 160.000000)">
+                <path d="M370.21875,484 C370.21875,483.448 370.68915,483 371.26875,483 C371.84835,483 372.31875,483.448 372.31875,484 C372.31875,484.552 371.84835,485 371.26875,485 C370.68915,485 370.21875,484.552 370.21875,484 L370.21875,484 Z M381.9,497 C381.9,497.552 381.4296,498 380.85,498 L379.8,498 L379.8,494 C379.8,492.895 378.86025,492 377.7,492 L369.3,492 C368.13975,492 367.2,492.895 367.2,494 L367.2,498 L366.15,498 C365.5704,498 365.1,497.552 365.1,497 L365.1,487.044 C365.1,486.911 365.15565,486.784 365.2533,486.691 L367.2,484.837 L367.2,486 C367.2,487.105 368.13975,488 369.3,488 L377.7,488 C378.86025,488 379.8,487.105 379.8,486 L379.8,482 L380.85,482 C381.4296,482 381.9,482.448 381.9,483 L381.9,497 Z M377.7,498 L369.3,498 L369.3,495 C369.3,494.448 369.7704,494 370.35,494 L376.65,494 C377.2296,494 377.7,494.448 377.7,495 L377.7,498 Z M369.3,482.837 L370.17885,482 L377.7,482 L377.7,485 C377.7,485.552 377.2296,486 376.65,486 L370.35,486 C369.7704,486 369.3,485.552 369.3,485 L369.3,482.837 Z M381.9,480 L369.7347,480 C369.45645,480 369.18975,480.105 368.99235,480.293 L363.30765,485.707 C363.11025,485.895 363,486.149 363,486.414 L363,498 C363,499.105 363.93975,500 365.1,500 L381.9,500 C383.06025,500 384,499.105 384,498 L384,482 C384,480.895 383.06025,480 381.9,480 L381.9,480 Z" id="save_item-[#1411]"></path>
+            </g>
+        </g>
+    </g>
+</svg>`,
+    command: 'save-data',
+    id: 'sava-data'
+  }]);
+
+
+  editor.value.Storage.add('remote', {
+    // async load() {
+    //   return await axios.get(projectEndpoint);
+    // },
+
+    async store(data) {
+      return await axios.post(projectEndpoint, { id: currentId.value, templateData: data }).then(
+        (response) => {
+          currentId.value = response.data.id
+        }
+      );
+    },
+  });
 
 
 
@@ -169,6 +232,13 @@ onMounted(() => {
     run: editor => editor.setDevice('A5')
   });
 })
+
+
+
+
+// pn.addButton('options', [{ id: 'save-database', className: 'fa fa-floppy-o', command: 'save-database', attributes: { title: 'Save to database' } }])
+
+
 
 
 </script>
